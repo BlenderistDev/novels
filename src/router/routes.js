8a22711c4f6fc5cd51baadb79873587cbbaa30c4
Anton Listkov <antonlistkov@gmail.com>
2021-02-09 19:38:46 +0300

    обновил routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,20 +2,17 @@
 const routes = [
   {
     path: '/',
+    component: () => import('pages/Main.vue'),
+    name: 'index'
+  },
+
+  {
+    path: '/editor',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
-
-  {
-    path: '/list',
-    component: () => import('pages/Main.vue'),
-    name: 'novels-list'
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '*',
     component: () => import('pages/Error404.vue')
